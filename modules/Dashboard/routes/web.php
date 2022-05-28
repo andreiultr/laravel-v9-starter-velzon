9fb6016d7323fe49c2a966091c6578bf7d88572c
b0rnt0ber00t <lanabackend@gmail.com>
2022-05-28 22:43:43 +0700

    module dashboard

--- modules/Dashboard/routes/web.php
+++ modules/Dashboard/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Dashboard\app\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/login');
+Route::resource('dashboard', DashboardController::class)->only('index');
