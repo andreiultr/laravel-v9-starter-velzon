ce47306221850347a4a0cedd0d834fbb89f6586e
b0rnt0ber00t <lanabackend@gmail.com>
2022-06-03 09:20:07 +0700

    add more permission

--- modules/PermissionManagement/database/seeders/PermissionSeederTableSeeder.php
+++ modules/PermissionManagement/database/seeders/PermissionSeederTableSeeder.php
@@ -25,10 +25,37 @@ class PermissionSeederTableSeeder extends Seeder
         Permission::create(['name' => 'general']);
         Permission::create(['name' => 'option']);
 
+        // dashboard
         Permission::create(['name' => 'dashboard.index']);
+
+        // menu group
         Permission::create(['name' => 'menu.index']);
+        Permission::create(['name' => 'menu.store']);
+        Permission::create(['name' => 'menu.update']);
+        Permission::create(['name' => 'menu.destroy']);
+
+        // menu items
+        Permission::create(['name' => 'menu.item.index']);
+        Permission::create(['name' => 'menu.item.store']);
+        Permission::create(['name' => 'menu.item.update']);
+        Permission::create(['name' => 'menu.item.destroy']);
+
+        // route management
         Permission::create(['name' => 'route.index']);
+        Permission::create(['name' => 'route.store']);
+        Permission::create(['name' => 'route.update']);
+        Permission::create(['name' => 'route.destroy']);
+
+        // role management
         Permission::create(['name' => 'role.index']);
+        Permission::create(['name' => 'role.store']);
+        Permission::create(['name' => 'role.update']);
+        Permission::create(['name' => 'role.destroy']);
+
+        // permission management
         Permission::create(['name' => 'permission.index']);
+        Permission::create(['name' => 'permission.store']);
+        Permission::create(['name' => 'permission.update']);
+        Permission::create(['name' => 'permission.destroy']);
     }
 }
--- modules/PermissionManagement/database/seeders/RouteSeederTableSeeder.php
+++ modules/PermissionManagement/database/seeders/RouteSeederTableSeeder.php
@@ -19,32 +19,112 @@ class RouteSeederTableSeeder extends Seeder
 
         // $this->call("OthersTableSeeder");
 
+        // Dashboard
         Route::insert([
             [
                 'route' => 'dashboard.index',
                 'permission_name' => 'dashboard.index'
             ],
+        ]);
+
+        // Menu Group
+        Route::insert([
             [
                 'route' => 'menu.index',
                 'permission_name' => 'menu.index'
             ],
             [
+                'route' => 'menu.store',
+                'permission_name' => 'menu.store'
+            ],
+            [
+                'route' => 'menu.update',
+                'permission_name' => 'menu.update'
+            ],
+            [
+                'route' => 'menu.destroy',
+                'permission_name' => 'menu.destroy'
+            ],
+        ]);
+
+        // Menu Item
+        Route::insert([
+            [
                 'route' => 'menu.item.index',
                 'permission_name' => 'menu.item.index'
             ],
             [
-                'route' => 'permission.index',
-                'permission_name' => 'permission.index'
+                'route' => 'menu.item.store',
+                'permission_name' => 'menu.item.store'
             ],
+            [
+                'route' => 'menu.item.update',
+                'permission_name' => 'menu.item.update'
+            ],
+            [
+                'route' => 'menu.item.destroy',
+                'permission_name' => 'menu.item.destroy'
+            ],
+        ]);
+
+        // route management
+        Route::insert([
+            [
+                'route' => 'route.index',
+                'permission_name' => 'route.index'
+            ],
+            [
+                'route' => 'route.store',
+                'permission_name' => 'route.store'
+            ],
+            [
+                'route' => 'route.update',
+                'permission_name' => 'route.update'
+            ],
+            [
+                'route' => 'route.destroy',
+                'permission_name' => 'route.destroy'
+            ],
+        ]);
+
+        // role management
+        Route::insert([
             [
                 'route' => 'role.index',
                 'permission_name' => 'role.index'
             ],
             [
-                'route' => 'route.index',
-                'permission_name' => 'route.index'
+                'route' => 'role.store',
+                'permission_name' => 'role.store'
             ],
+            [
+                'route' => 'role.update',
+                'permission_name' => 'role.update'
+            ],
+            [
+                'route' => 'role.destroy',
+                'permission_name' => 'role.destroy'
+            ],
+        ]);
 
+        // permission management
+        Route::insert([
+            [
+                'route' => 'permission.index',
+                'permission_name' => 'permission.index'
+            ],
+            [
+                'route' => 'permission.store',
+                'permission_name' => 'permission.store'
+            ],
+            [
+                'route' => 'permission.update',
+                'permission_name' => 'permission.update'
+            ],
+            [
+                'route' => 'permission.destroy',
+                'permission_name' => 'permission.destroy'
+            ],
         ]);
     }
 }
