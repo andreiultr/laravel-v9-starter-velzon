80a7a05d5c3e5fadc9e9321bdbddb7efecb5f376
b0rnt0ber00t <lanabackend@gmail.com>
2022-06-17 22:44:36 +0700

    update sidebar permission

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -39,6 +39,6 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-        ])->assignRole($role);
+        ])?->assignRole($role);
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,9 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
-            // custom by developer
-            \App\Http\Middleware\RouteMiddleware::class,
+            'route.permission'
         ],
 
         'api' => [
@@ -66,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // custom by developer
+        'route.permission' => \App\Http\Middleware\RouteMiddleware::class,
     ];
 }
